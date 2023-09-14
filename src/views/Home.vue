f5e9bff418671412b98f4a0a39e52a5e532e270b
jamesulip <ulip.james@gmail.com>
2023-09-14 19:23:08 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    asd</div>
+  <!-- <HelloWorld /> -->
+</template>
+
+<script lang="ts" setup>
+// import HelloWorld from '@/components/HelloWorld.vue'
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+// unplugin-vue-components
+import Components from 'unplugin-vue-components/vite'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+    Components({
+      dts:'src/components.d.ts',
+      directoryAsNamespace: true,
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+})
